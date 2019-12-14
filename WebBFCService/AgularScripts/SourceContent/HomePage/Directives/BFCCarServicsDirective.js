b720950d0a4df71dd8ac2fa60c881e615f26c25c
Pavan Narasimha SV <pavan9985424457@gmail.com>
2019-12-15 01:13:38 +0530

    Added Js Each Induvidual tab of View Profile, Contact, FeedBack buttons

---
+++ WebBFCService/AgularScripts/SourceContent/HomePage/Directives/BFCCarServicsDirective.js
@@ -0,0 +1,11 @@
+﻿BFCHomePage.directive('bfcCarServiceDirective', function () {
+    return ({
+        restrict: 'AE',
+        transclude: true,
+        replace: true,
+        templateUrl: "Views/bfcEachServicesProfile/bfcCarServicesButViewContProf.html",
+        cssUrl: "Styles/CSS/BFCViewProfileContactFeeBack.css",
+        controller: "",//controller to bind
+        controllerAs: "vm", bindToController: true,//
+    })
+});
