da95dbd30d0057e4d5b51b4f4c4a50abe0d1818b
Pavan Narasimha SV <pavan9985424457@gmail.com>
2019-12-17 01:02:27 +0530

    Testing the Main Profile View

---
+++ WebBFCService/AgularScripts/SourceContent/bfcMainProfileView/Directives/bfcCarMainProfileView.js
@@ -0,0 +1,17 @@
+﻿BFCHomePage.directive('bfcCarMainProfileView', function () {
+    return ({
+        restrict: 'AE',
+        transclude: true,
+        replace: true,
+        templateUrl: 'Views/bfcProfileViewDivs/bfcCarServicesProfileView.html',
+        controller: "BFCCarMainProfileViewCtrl",//controller to bind
+        controllerAs: "vm", bindToController: true,//
+        link: function (scope, elm, attrs, ctrl) {
+
+        }
+    })
+}).controller("BFCCarMainProfileViewCtrl", ['$scope', function ($scope) {
+
+
+
+}]);
