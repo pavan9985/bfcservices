7b7478aa8a70032454cf716f54d24625d1989b38
Pavan Narasimha SV <pavan9985424457@gmail.com>
2020-01-04 20:53:48 +0530

    added the chat boot in main page

---
+++ WebBFCService/AgularScripts/SourceContent/HomePage/Directives/BfcChatboot/BfcChatBootDirective.js
@@ -0,0 +1,19 @@
+﻿BFCHomePage.directive("bfcChatBootDirective", function () {
+    return ({
+        restrict: 'AE',
+        transclude: true,
+        replace: true,
+        templateUrl: 'Views/BfcChatBoot/BFCChatBootPopUp.html',
+        controller: "BfcChatBootController",//controller to bind
+        controllerAs: "vm", bindToController: true,//
+        link: function (scope, elm, attrs, ctrl) {
+
+        }
+    })
+
+}).controller("BfcChatBootController", ['$scope', function ($scope) {
+    $scope.CrossCancleChatBoot = function () {
+        $scope.BfcChatbootShowHide = true;
+        $scope.BfcChatbootHomePageIcon = false;
+    };
+}]);
