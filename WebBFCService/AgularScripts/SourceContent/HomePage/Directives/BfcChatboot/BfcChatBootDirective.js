2be21d89b1da4429bdf2bbcb1244b6a9fc7be13e
Pavan Narasimha SV <pavan9985424457@gmail.com>
2020-01-16 03:44:15 +0530

    added refresh Token every one hour in chatbot

--- WebBFCService/AgularScripts/SourceContent/HomePage/Directives/BfcChatboot/BfcChatBootDirective.js
+++ WebBFCService/AgularScripts/SourceContent/HomePage/Directives/BfcChatboot/BfcChatBootDirective.js
@@ -12,9 +12,13 @@
     })
 
 }).controller("BfcChatBootController", ['$scope', function ($scope) {
-
-    var GooleDilogFlowBaseURL = "https://dilogflow.googleapis.com/v2/";
-    var GoolgeDiglogFlowClientToken = "334572477511-nd15dr112ttp11aidphh8f3uh47cen01.apps.googleusercontent.com";
+    var clientId = "334572477511-h0mfjbn0a1behm2ja63dcls65jn91k11.apps.googleusercontent.com";
+    var clientSecret = "vAMNij5ZWH7gZAEvX9TprxbC";
+    var tokenKey = "ya29.Il-6B-3CPrP98riYEi93ha8ptVsXPEo2RwCoLP9i-i_GPJ6j7MA2xPU7wsyH6qCgPiZofnodNfbusRQvIJRC6VGWGZSTC8DUXQruBH7Ho0uplDXs_NNF3oWCZaBTQfOQw";
+    var RefreshToken = "1//0gV_SwwZ0Rs1vCgYIARAAGBASNwF-L9IrN31wEty5KFzh2jagusiO7WmnHjkL-xlKugKUwRaKkjZ2zvNiNnrXUca7HA1cN2_KPwI";
+    var refreshAccessTokenUrl = "https://accounts.google.com/o/oauth2/token";
+    var dilogFlowGetIndentUrl = "https://dialogflow.googleapis.com/v2beta1/projects/bfcservices-vdshos/agent/sessions/756ce29c-80ba-069d-a0d1-72d53396545:detectIntent";
+    //var GoolgeDiglogFlowClientToken = "334572477511-nd15dr112ttp11aidphh8f3uh47cen01.apps.googleusercontent.com";
 
 
     $scope.CrossCancleChatBoot = function () {
@@ -52,35 +56,67 @@
         addSendClassDiv(ChatMessagetoSend);
 
         $scope.ChatBootMessagetoSendText = '';
-        GoogleDilogFlowService(ChatMessagetoSend);
+        GoogleDilogFlowService({ query_input: { text: { text: ChatMessagetoSend, language_code: "en-US" } }, queryParams: { timeZone: "Asia/Calcutta" } }, dilogFlowGetIndentUrl, "application/json; charset-utf-8", "json", { 'Authorization': "Bearer " + tokenKey });
     };
 
-    function init() {
-
-    }
-
     //gapi.load('client', start);
-    function GoogleDilogFlowService(ChatMessagetoSend) {
+    function GoogleDilogFlowService(data, url, ContentType, dataType, hedders) {
+        var PostData;
 
-        if (!hasValue(ChatMessagetoSend)) {
-            return;
+        if (dataType == "json") {
+            PostData = JSON.stringify(data);
+        }
+        else if (dataType == "Text") {
+            PostData = data;
         }
 
         $.ajax({
             type: "POST",
-            url: "https://dialogflow.googleapis.com/v2beta1/projects/bfcservices-vdshos/agent/sessions/756ce29c-80ba-069d-a0d1-72d53396b825:detectIntent",
-            contentType: "application/json; charset-utf-8",
-            dataType: "json",
-            headers: {
-                'Authorization': "Bearer ya29.c.Kl65B2ZL7XJXN8GZQ6IGrLolsA-u3aDh-IkMFvDu_2pnGzSj_xKnWzQ7QJ7N_-wiZaIu3sDYBQ1SlUryhx-P7EhqSQtPFkNAB8XBvTo1ayDZzSV7jqqeEDNDDtP81z7I"
-            },
-            data: JSON.stringify({
-                "query_input": { "text": { "text": ChatMessagetoSend, "language_code": "en-US" } }, "queryParams": { "timeZone": "Asia/Calcutta" }
-            }),
+            url: url,
+            contentType: ContentType,
+            dataType: dataType,
+            headers: hedders,
+            data: PostData,
             success: function (data) {
+                if (hasValue(data.access_token)) {
+                    tokenKey = data.access_token;
+                }
                 addReceiverClassDiv(data);
-            }
+            },
+            error: function (erroeResponse) {
+                console.log(erroeResponse.status);
+                if (erroeResponse.status === 401) {
+                    getRefrestToken();
+                }
+            },
+            dataType: "json",
+            contentType: "application/json"
         });
+
+
+        //$.ajax({
+        //    type: "POST",
+        //    url: "https://dialogflow.googleapis.com/v2beta1/projects/bfcservices-vdshos/agent/sessions/756ce29c-80ba-069d-a0d1-72d53396b825:detectIntent",
+        //    contentType: "application/json; charset-utf-8",
+        //    dataType: "json",
+        //    headers: {
+        //        'Authorization': "Bearer ya29.c.Kl65B1P2Jm_0d0qdrk4ARQLQsxTmcz_b1xicwPEPTpWZByJHipS2AXUJHm6MSI-Dx0BG-5AyMWTY7YB6-i58XiOai-6VH8WnKgl-mKQKP1aoe0yUHTA6JeUH6plkXl-r"
+        //    },
+        //    data: JSON.stringify({
+        //        "query_input": { "text": { "text": ChatMessagetoSend, "language_code": "en-US" } }, "queryParams": { "timeZone": "Asia/Calcutta" }
+        //    }),
+        //    success: function (data) {
+        //        addReceiverClassDiv(data);
+        //    },
+        //    error: function (erroeResponse) {
+        //        console.log(erroeResponse.status);
+        //        if (erroeResponse.status === 401) {
+        //            getRefrestToken();
+        //        }
+        //    },
+        //    dataType: "json",
+        //    contentType: "application/json"
+        //});
 
 
         //$.ajax({
@@ -149,5 +185,8 @@
             scrollTop: document.getElementById('ParrentDivofChatbootmessages').scrollHeight - document.getElementById('ParrentDivofChatbootmessages').clientHeight
         }, 200);
     }
-
+    function getRefrestToken() {
+        //GoogleDilogFlowService("client_id=" + clientId + "&client_secret=" + clientSecret + "&refresh_token=" + RefreshToken + "&grant_type=refresh_token", refreshAccessTokenUrl, "application / x - www - form - urlencoded", "Text", undefined);
+        GoogleDilogFlowService({ client_id: clientId, client_secret: clientSecret, refresh_token: RefreshToken, grant_type: "refresh_token" }, refreshAccessTokenUrl, "application/json; charset-utf-8", "json", undefined);
+    }
 }]);
