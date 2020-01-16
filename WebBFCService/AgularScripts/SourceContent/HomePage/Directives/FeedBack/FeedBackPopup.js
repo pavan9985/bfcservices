ab736a434e6d5ca7159bc889bd763aabe4e8550a
Pavan Narasimha SV <pavan9985424457@gmail.com>
2020-01-17 03:09:03 +0530

    Added the Feed Back Form

---
+++ WebBFCService/AgularScripts/SourceContent/HomePage/Directives/FeedBack/FeedBackPopup.js
@@ -0,0 +1,32 @@
+﻿BFCHomePage.directive("bfcFeedbackDirective", function () {
+    return ({
+        restrict: 'AE',
+        transclude: true,
+        replace: true,
+        templateUrl: 'Views/FeedBack/FeedBackPopup.html',
+        controller: "BfcFeedbackController",//controller to bind
+        controllerAs: "vm", bindToController: true,//
+        link: function (scope, elm, attrs, ctrl) {
+
+        }
+    })
+
+}).controller("BfcFeedbackController", ['$scope', function ($scope) {
+
+    var OpenTableOnFeedBackone;
+
+    $scope.CrossCancleFeedBack = function () {
+        if (OpenTableOnFeedBackone == 1) {
+            $scope.bfcHomePageViewHideBodyContent = false;
+            $scope.FeedBackDirective = true;
+        }
+        else if (OpenTableOnFeedBackone == 2) {
+            $scope.bfcMainProfileViewHideDirective = false;
+            $scope.FeedBackDirective = true;
+        }
+    };
+
+    $scope.OpenFeedBackOnTab = function (opneFeedBackTab) {
+        OpenTableOnFeedBackone = opneFeedBackTab;
+    }
+}]);
