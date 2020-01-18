b8a5c47857920e9e9acac04350059cc381e28f39
Pavan Narasimha SV <pavan9985424457@gmail.com>
2020-01-19 02:48:20 +0530

    Added only number in the Feed Back Phone Number Filed

--- WebBFCService/AgularScripts/SourceContent/CommonScript/CommonSource.js
+++ WebBFCService/AgularScripts/SourceContent/CommonScript/CommonSource.js
@@ -12,3 +12,24 @@ function GotoTop() {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
 }
+
+// **********************   Allow Only Number In the Input Field Starts.   ****************
+function validateOnlyNumbers(evt) {
+    var theEvent = evt || window.event;
+
+    // Handle paste
+    if (theEvent.type === 'paste') {
+        key = event.clipboardData.getData('text/plain');
+    } else {
+        // Handle key press
+        var key = theEvent.keyCode || theEvent.which;
+        key = String.fromCharCode(key);
+    }
+    var regex = /[0-9]|\./;
+    if (!regex.test(key)) {
+        theEvent.returnValue = false;
+        if (theEvent.preventDefault) theEvent.preventDefault();
+    }
+}
+
+// **********************   Allow Only Number In the Input Field Ends.   ****************
